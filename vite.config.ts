d424e1ed79bfd09d172b3f9892c04bd499fba9f7
shinjjoo <shinjjoo86@gmail.com>
2026-09-03 10:34:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/baegotdongsan2/',
+    base: '/bdc_fri/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
